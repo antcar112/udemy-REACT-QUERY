4a38413fc9a9af8c3f2bcba775d5b3fb1a860ada
Anthony Caron <antcar1992@gmail.com>
2021-05-23 14:52:53 -0700

    Add global error handler

--- lazy-days-spa/client/src/components/treatments/hooks/useTreatments.ts
+++ lazy-days-spa/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,17 +3,14 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
-// for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
   const { data } = await axiosInstance.get('/treatments');
   return data;
 }
 
 export function useTreatments(): Treatment[] {
-  const fallback = [];
-
+  const fallback: Treatment[] = [];
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
 
   return data;
